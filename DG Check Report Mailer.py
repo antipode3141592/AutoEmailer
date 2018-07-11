0e716a4263f7852b4ae44f70af345ec5fe792dab
Sean Kirkpatrick <skirkpatrick@racc.org>
2018-07-10 17:20:36 -0700

    Added automailer with direct db connection (instead of reading excel file for contact info)

--- DG Check Report Mailer.py
+++ DG Check Report Mailer.py
@@ -2,8 +2,9 @@
 #   Copyright 2018 by Sean Vo Kirkpatrick using GNU GPL v3
 #   skirkpatrick@racc.org or sean@studioantipode.com or seanvokirkpatrick@gmail.com
 #
-#   Sends an e-mail with an attachment, based on the file name and a lookup table in an excel file,
-#   for each attachment file in a given input folder.
+#   Sends an e-mail with an attachment.  To send files, place the attachments in the input folder and
+#       run the script - this will read the Raiser's Edge database for the contact information related to the
+#       organization name in the front half of the input file
 #   
 #   Requirements:     
 #               -a running and e-mail ready Outlook instance
@@ -19,15 +20,16 @@
 #   Makes a connection to an open instance of Outlook, e-mails will be sent using the active account
 #   The program loops through all of the files in the "path_input" folder:
 #       First, parse the file name name to get the organization name.
-#       Second, search the contact list excel file for the appropriate e-mail addresses.
+#       Second, search the database results for all e-mail addresses associated with that organization
 #       Send the file as an attachment to all e-mail addresses found, with a body set to the "file_emailbody" file
 #       (note: currently only plain text)
 #   Files that have a positive number of associated e-mail addresses are then moved to the "path_output" folder, 
 #       and files that had no associated e-mail addresses to the "path_errors" folder.
-
 #
-#   Tested using    - Anaconda 5.0.0
-#                   - pandas 0.22.0
+#   Tested using    - Anaconda 5.1.0
+#                   - pandas 0.23.1
+#                   - pyodbc 4.0.23
+#                   - numpy 1.14.5
 #   IDE: Visual Studio 2017 Community Edition
 
 # License Info:
@@ -41,55 +43,49 @@
 #    GNU General Public License for more details.
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 import win32com.client as win32
+import pyodbc 
 import os
+import numpy as np
 import pandas as pd
 
+#small function for replacing restricted characters in filenames
 def sterilizestring (s):
     for char in "?.!/;:":
         s = s.replace(char,'_');
     return s
 
-outlook = win32.DispatchEx('outlook.application')   #TODO: modify to use any e-mail, not just control Outlook
-
+cnxn = pyodbc.connect("Driver={SQL Server Native Client 11.0};" #requires explicitily stating the sql driver
+                      "Server=overlook;"
+                      "Database=re_racc;"
+                      "Trusted_Connection=yes;")    #use windows integrated security
+cursor = cnxn.cursor()
+#note: this should be a stored procedure
+cursor.execute("select a.DESCRIPTION, a.FUND_ID, b.LONGDESCRIPTION as Category, d.CONSTITUENT_ID, d.ORG_NAME, e.num as Email, f.LONGDESCRIPTION as Type from FUND a join TABLEENTRIES b on (a.FUND_CATEGORY = b.TABLEENTRIESID) join FUND_ORG_RELATIONSHIPS c on (a.ID = c.FUND_ID) join RECORDS d on (c.CONSTIT_ID = d.ID) join PHONES e on (e.CONSTIT_ID = d.ID) join TABLEENTRIES f on (e.PHONETYPEID =  f.TABLEENTRIESID) where b.LONGDESCRIPTION = 'Designated' and f.LONGDESCRIPTION like 'E-Mail%' and e.CONSTIT_RELATIONSHIPS_ID is null order by a.DESCRIPTION, f.LONGDESCRIPTION")
+data = []   #grab results, put into a list, put list into numpy array, and then put numpy array into pandas dataframe
+for row in cursor:
+    data.append(tuple(row))
+contactlist = pd.DataFrame.from_records(np.array(data))
+outlook = win32.DispatchEx('outlook.application')
 #file paths
 path_root = "C:\\Users\\skirkpatrick\\Documents\\Work for Art\\DG Pledge Notification Reports\\"
-#path_contactlist = "//concordia/lancentral/Work for Art/Designated Gifts/DG Pledge Reports//"   #network locations use forward slashes
-path_contactlist = "//concordia/users/skirkpatrick/Export//"
-
-#path_input = "C:\\Users\\skirkpatrick\\Documents\\Work for Art\\DG Pledge Notification Reports\\Input\\"
-path_input = "C:\\Users\\skirkpatrick\\Coding\\Python\\Outgoing\\"
-path_output = "C:\\Users\\skirkpatrick\\Coding\\Python\\Processed\\"
-#path_output = "C:\\Users\\skirkpatrick\\Documents\\Work for Art\\DG Pledge Notification Reports\\Output\\"
-path_errors = "C:\\Users\\skirkpatrick\\Coding\\Python\\Errors\\"
-#path_errors = "C:\\Users\\skirkpatrick\\Documents\\Work for Art\\DG Pledge Notification Reports\\Errors\\"
-file_emailbody = path_root + "dg payment email.txt"
-#file_contactlist = path_contactlist + "DG Arts Org Email List for Pledge reports.xlsx"
-file_contactlist = path_contactlist + "funds and emails.xlsx"
-
-#load data
+path_input = "C:\\Users\\skirkpatrick\\Documents\\Work for Art\\DG Pledge Notification Reports\\Input\\"
+path_output = "C:\\Users\\skirkpatrick\\Documents\\Work for Art\\DG Pledge Notification Reports\\Output\\"
+path_errors = "C:\\Users\\skirkpatrick\\Documents\\Work for Art\\DG Pledge Notification Reports\\Errors\\"
+file_emailbody = path_root + "email template.txt"
 message_body = open(file_emailbody).read()  #load message body from file
-wb = pd.ExcelFile(file_contactlist)
-df1 = wb.parse('funds_and_emails')  #parse contents of excel file into Pandas dataframe
-
 #initialize lists
 files_good = []
 files_error = []
-
 #email loop
 for f in os.listdir(path_input):
-    _f = f.split(".")
+    _f = f.split(" - ")
     validemailacount = 0
     emaillist = []      #list of e-mails to send to (each item might need to be mailed to multiple addresses)
-    for index, row in df1.iterrows():   #iterate over all rows, searching for matches
-        #print("org " + row[0])
+    for index, row in contactlist.iterrows():   #iterate over all rows, searching for matches
         if sterilizestring(row[0]) == sterilizestring(_f[0]):             #if Org Name matches File Name, add e-mail to list
-            for i in [4,5,6,7]:
-                #print(row[i])
-                if row[i] == row[i]:
-                    emaillist.append(row[i])
-                    validemailacount += 1
+            emaillist.append(row[5])
+            validemailacount += 1
     if validemailacount == 0:    #if no valid matches, print error and save filename in error list
         files_error.append(f)
         print(f, " not found!")
@@ -103,11 +99,10 @@ for f in os.listdir(path_input):
         print("Sending email to: ", stringemail)
         mail = outlook.CreateItem(0)
         mail.To = stringemail
-        mail.Subject = 'Work for Art - Designated Fund Quarterly Payment Report'
+        mail.Subject = 'Work for Art - New Pledges through Work for Art'
         mail.Body = message_body
         mail.Attachments.Add(path_input + f) #f is the excel report filename
         mail.Send()
-
 #file moving loops
 for f in files_good:
     os.rename(path_input + f, path_output + f)
